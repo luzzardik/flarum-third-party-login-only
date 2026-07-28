6aaa449f258a0b01a4515117cf8b1518d1ff6ddb
Kacy Luzzardi <kacy@kume.co>
2026-07-28 23:06:07 +0200

    Revert changes

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,23 +1,17 @@
 import app from "flarum/forum/app";
 import { extend, override } from "flarum/common/extend";
 
-// 1. Import all the components you need to extend or use
 import LogInButton from "flarum/forum/components/LogInButton";
-import LogInModal from "flarum/forum/components/LogInModal";
-import SignUpModal from "flarum/forum/components/SignUpModal";
-import HeaderSecondary from "flarum/forum/components/HeaderSecondary";
-import SettingsPage from "flarum/forum/components/SettingsPage";
 
 app.initializers.add("luzzardik-flarum-third-party-login-only", () => {
-  // 2. Extend the prototype of the imported classes
-  extend(LogInModal.prototype, "fields", function (items) {
+  extend("flarum/forum/components/LogInModal", "fields", function (items) {
     items.remove("identification");
     items.remove("password");
     items.remove("remember");
     items.remove("submit");
   });
 
-  extend(SignUpModal.prototype, "fields", function (items) {
+  extend("flarum/forum/components/SignUpModal", "fields", function (items) {
     if (this.attrs.token && app.forum.attribute("signUpWelcomeText")) {
       items.add(
         "welcome-message",
@@ -37,7 +31,7 @@ app.initializers.add("luzzardik-flarum-third-party-login-only", () => {
     items.remove("password");
   });
 
-  override(LogInModal.prototype, "footer", function () {
+  override("flarum/forum/components/LogInModal", "footer", function () {
     if (app.forum.attribute("forgotPasswordLink") === "") {
       return null;
     }
@@ -55,30 +49,39 @@ app.initializers.add("luzzardik-flarum-third-party-login-only", () => {
     );
   });
 
-  extend(HeaderSecondary.prototype, "items", function (items) {
+  extend("flarum/forum/components/HeaderSecondary", "items", function (items) {
     if (!app.forum.attribute("replaceLoginWithFoFPassport")) {
       return;
     }
 
-    // 3. Replace .component() with standard JSX syntax
     if (app.forum.attribute("allowSignUp")) {
       items.replace(
         "signUp",
-        <LogInButton className="Button Button--link" path="/auth/passport">
-          {app.translator.trans("core.forum.header.sign_up_link")}
-        </LogInButton>
+        LogInButton.component(
+          {
+            className: "Button Button--link",
+            path: "/auth/passport",
+          },
+          app.translator.trans("core.forum.header.sign_up_link")
+        )
       );
     }
 
     items.replace(
       "logIn",
-      <LogInButton className="Button Button--link" path="/auth/passport">
-        {app.translator.trans("core.forum.header.log_in_link")}
-      </LogInButton>
+      LogInButton.component(
+        {
+          className: "Button Button--link",
+          path: "/auth/passport",
+        },
+        app.translator.trans("core.forum.header.log_in_link")
+      )
     );
   });
 
-  extend(SettingsPage.prototype, "accountItems", function (items) {
+  extend("flarum/forum/components/SettingsPage", "accountItems", function (
+    items
+  ) {
     if (app.forum.attribute("changePasswordLink")) {
       items.replace(
         "changePassword",
