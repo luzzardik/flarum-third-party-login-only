b4b2ca3b051f9e09d03b55f7c29ce20086d48575
Kacy Luzzardi <kacy@kume.co>
2026-07-28 22:41:18 +0200

    Use extend.tsx rather than index.ts

---
+++ js/src/admin/index.ts
@@ -0,0 +1,7 @@
+// admin/index.ts
+export { default as extend } from './extend';
+
+app.initializers.add('luzzardik-third-party-login-only', () => {
+  // Imperative-only logic goes here. Keep your settings, permissions,
+  // and page registration in extend.ts.
+});
